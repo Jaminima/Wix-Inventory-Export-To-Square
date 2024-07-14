c5aff70420ba93fc8090be6bd84c5e4b392d150c
Oscar Davies <oscar@designlogica.co.uk>
2024-07-14 13:21:10 +0100

    Add project files.

---
+++ WIXToSquareInventory/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace WIXToSquareInventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No file arguments provided.");
+                return;
+            }
+
+            string inputFilePath = args[0];
+
+            if (!File.Exists(inputFilePath))
+            {
+                Console.WriteLine($"File {inputFilePath} does not exist.");
+                return;
+            }
+
+            string outputFilePath = Path.Combine(Path.GetDirectoryName(inputFilePath), "output.csv");
+
+        }
+    }
+}
